231bf22f675d0b2f0321e1b14d0ab6612d1cde1e
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-22 15:15:53 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   if board[index] = "X" || board[index] = "O"
     true
   elsif board[index] = "" || board[index] = " " || board[index] = nil
-    return false
+    false
   else
     nil
   end
